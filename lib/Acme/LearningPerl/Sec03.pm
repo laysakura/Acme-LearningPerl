521033d3ce37af1d59bf3e0646d9d0b49d715ecb
Sho Nakatani <lay.sakura@gmail.com>
2014-04-01 22:32:40 +0900

    simpler answer for Sec03

--- lib/Acme/LearningPerl/Sec03.pm
+++ lib/Acme/LearningPerl/Sec03.pm
@@ -9,12 +9,7 @@ use File::Spec::Functions;
 
 sub ex1 {
     print "Input list of strings (Ctrl-D to end)\n";
-    my @strlist = ();
-    while (<STDIN>) {
-        chomp;
-        push @strlist, $_;
-    }
-    print reverse "@strlist";
+    print reverse <STDIN>;
 }
 
 sub ex2 {
@@ -27,11 +22,7 @@ sub ex2 {
 
 sub ex3 {
     print "Input list of strings (Ctrl-D to end)\n";
-    my @strlist = ();
-    while (<STDIN>) {
-        push @strlist, $_;
-    }
-    print sort @strlist;
+    print sort <STDIN>;
 }
 
 1;
